f8ab867e1cbf847cae181d184470f92c41261ec6
Broot <Prisepan45@gmail.com>
2023-09-27 04:12:19 +0500

    Сбор данных (неполный)

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-##import struct
-
-#for byte in b"\x41\x41\x00\x00":
-
-#adress = int(input("Введите адрес устройства: "))
-#adress = hex(adress)[2:]
+import struct
 
 #print(struct.pack(">hh", 10, 12))
 #print((str(struct.pack("h", 1022))[4:-1:]).split("\\x"))
@@ -40,3 +35,28 @@
 #mas.extend([4, 5, 6]) #>>> [1, 2, 3, 4, 5, 6]
 #mas.reverse() #>>> [6, 5, 4, 3, 2, 1]
 #print(mas)
+
+oldSet2 = [45, 3, 4, 65, 96, 101, 60, 40, 146]
+
+def CRC16(data: list): # Функция, вычисляющая контрольную сумму по методу CRC16, контрольная сумма идёт в конце каждого пакета
+    poly = 0xa001 # На вход подаётся массив байт прямого порядка
+    xor1 = 0x0000
+    crc = 0xffff
+    len_ = len(data)
+    i = 0
+
+    while len_: 
+        len_ -= 1
+        crc ^= (data[i])
+        i += 1        
+        
+        for j in range(0, 8):    
+            if crc & 0x0001:
+                crc = (crc >> 1) ^ poly
+            else:
+                crc = crc >> 1
+            
+    crc ^= xor1
+    return crc # На выход получаем число, пе
+
+print(CRC16(oldSet2))
